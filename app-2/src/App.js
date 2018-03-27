37458f5482b28eec8bf9739e7107a3bbd67ab22a
Hayden <anhazzing33@gmail.com>
2018-03-27 17:50:39 -0500

    finished till app-4

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,7 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      foods:[
+        "spaghetti",
+         "ice cream",
+         "sushi",
+         "cheese"
+      ]
+    };
+  }
   render() {
+    let foodsToDisplay = this.state.foods.map((el,i)=>{
+      return (
+        <h2 key={i}>
+        {el}
+        </h2>
+      )
+    })
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +31,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {foodsToDisplay}
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,7 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterString: "",
+      foods: [
+        "spaghetti",
+        "ice cream",
+        "sushi",
+        "bologna",
+        "cheese"
+      ]
+    }
+  }
+handleChange(filter){
+  this.setState({filterString : filter})
+}
   render() {
+    const {foods,filterString} = this.state;
+    let foodsTodisplay = foods.filter((el,i)=>{
+        return el.includes(filterString)
+    }).map((el,i)=>{return (
+      <h2 key ={i}>{el}</h2>
+    )})
     return (
       <div className="App">
         <div className="App-header">
@@ -13,7 +35,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+     
+      <input
+      onChange ={(e)=>{this.handleChange(e.target.value)}}
+      type ="text"
+      />
+      {foodsTodisplay}
+       </div>
     );
   }
 }
